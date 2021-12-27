ad82d5070c3d9b5129e307d050deaf8cbbd60edd
mst <rudpajiuck@gmail.com>
2021-12-27 12:43:13 +0200

    Quill now fully supported

--- app/javascript/controllers/quill_controller.js
+++ app/javascript/controllers/quill_controller.js
@@ -2,10 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 import Quill from "quill"
 
 export default class extends Controller {
+  static targets = [ "editor" ]
+
+  fill() {
+    document.getElementById('article_body').value = document.querySelector('.ql-editor').innerHTML
+  }
+
   connect() {
-    var editor = new Quill('#editor', {
-      modules: { toolbar: '#toolbar' },
-      theme: 'snow'
+    // Quill settings
+    var toolbarOptions = [
+      [{ 'header': 1 }],
+      ['code-block'],
+      ['italic', 'underline', 'strike'],
+      ['link'],
+      ['image']
+    ];
+
+    var container = document.getElementById('editor');
+
+    var quill = new Quill(container, {
+      theme: 'bubble',
+      placeholder: 'Write',
+      modules: {
+        toolbar: toolbarOptions
+      }
     });
+
+    // fill Quill editor from article value in edit viev
+    document.querySelector('.ql-editor').innerHTML = document.getElementById('article_body').value;
+
   }
 }
